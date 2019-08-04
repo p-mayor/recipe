2a4f028beec337c0215af7647955233a9c484282
p-mayor <petermayor@kenzie.academy>
2019-08-04 12:12:31 -0400

    init

---
+++ recipe/recipe/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=20)
+    bio = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=20)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    instructions = models.TextField("Instructions", null=True, blank=True)
+    def __str__(self):
+        return self.title
