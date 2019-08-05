2308a4d9d7e7f5a82a2a6eb414e70243e79c7cad
p-mayor <petem14@gmail.com>
2019-08-04 23:11:19 -0400

    rearrange dir structure / update urls

--- recipe/recipe/models.py
+++ recipe/recipe/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=20)
-    bio = models.CharField(max_length=200)
+    bio = models.TextField("Bio", null=True, blank=True)
     def __str__(self):
         return self.name
 
@@ -12,6 +12,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=20)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
+    time_req = models.CharField(max_length=20)
     instructions = models.TextField("Instructions", null=True, blank=True)
     def __str__(self):
         return self.title
