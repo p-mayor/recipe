04fa429874c588bb506c26835574f03243dafef7
p-mayor <petem14@gmail.com>
2019-08-05 00:32:01 -0400

    recipes are rendering

--- recipe/recipe/views.py
+++ recipe/recipe/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
+from .models import Recipe
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the recipe index.")
+    latest_recipe_list = Recipe.objects.order_by('title')
+    context = {'latest_recipe_list': latest_recipe_list}
+    return render(request, 'recipe/index.html', context)
+
+def detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, 'recipe/detail.html', {'recipe': recipe})
+
+def author(request, recipe_id):
+    return HttpResponse("The author page for %s." % recipe_id)
