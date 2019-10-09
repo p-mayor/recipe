259334af39574778df850bfefc37cf86d30e62c0
petem14 <petem14@gmail.com>
2019-10-09 17:00:49 -0400

    update author to have onetoone with admin/user

--- recipe/recipe/models.py
+++ recipe/recipe/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Author(models.Model):
+    user=models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     name = models.CharField(max_length=20)
     bio = models.TextField("Bio", null=True, blank=True)
     def __str__(self):
